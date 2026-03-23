ba1fd2b3e5ef3e18f2b146e9716dbdbb3548065a
majddiab33-design <majd.diab33@gmail.com>
2026-03-23 12:29:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(cors({
-  origin: true,
+  origin: [
+    "client-jqv4tjoan-majddiab33-9273s-projects.vercel.app",
+    "client-seven-rho-62.vercel.app",
+    "http://localhost:5173"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 
